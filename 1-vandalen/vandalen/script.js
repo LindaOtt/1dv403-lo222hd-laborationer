542fc3cd3a526619a00dc4a8c3b0d3744a674bdd
lindaott <lo222hd@student.lnu.se>
2014-11-20 09:40:21 +0000

    1.4 Getting names

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,43 @@
 "use strict";
 
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
 var makePerson = function(persArr){
-
-
-	// Din kod här...
-
+    
+    
+    /*
+    function logArrayElements(obj) {
+        Object.keys(obj).forEach(function (key) {
+            var foo = obj[key];
+            alert(foo);
+        });
+    }
+        
+    persArr.forEach(logArrayElements);
+    */
+    
+    var names = "";
+    persArr.forEach( function (arrayItem)
+    {
+        names += arrayItem.name;
+        names += ", ";
+    });
+    
+    return names;
+    
+    /*
+    //Objekt som returneras
+    var returnObj = {
+        minAge: 36,
+        maxAge: 46,
+        averageAge: 40,
+        names: "Johan Leitet, John Häggerud, Mats Loock"
+    };
+    
+    return returnObj;
+    */
 }
 
+var result = makePerson(data);
+
+console.log(result);
