7710f9a3cb999bb5e7da685327f2b27f64e7a139
lindaott <lo222hd@student.lnu.se>
2014-11-29 13:51:29 +0000

    2.2 done

--- 2-labbymezzage/js/message.js
+++ 2-labbymezzage/js/message.js
@@ -1,4 +1,39 @@
 /* Skapar en konstruktorfunktion */
-function Message(){
+function Message(message, date){
+    //Skapar privata getters och setters (privilegierade metoder)
+    //för meddelanden
+    
+    //Hämtar meddelandetexten
+    this.getText = function() {
+        return message;
+    }
+    
+    //Sätter meddelandetexten
+    this.setText = function(_text) {
+        message = _text;
+    }
+    
+    //Skapar privata getters och setters (privilegierade metoder)
+    //för datum
+    
+    //Hämtar datum
+    this.getDate = function() {
+        return date;
+    }
+    
+    //Sätter datum
+    this.setDate = function(_date) {
+        date = _date;
+    }
+    
+    //Strängrepresentation av objektet
+    Message.prototype.toString = function() {
+        return this.getText() + " (" + this.getDate()+")";
+    }
+    
+    //Hämtar texten med \n utbytt mot <br>
+    Message.prototype.getHTMLText = function() {
+        
+    }
     
 }
--- 2-labbymezzage/js/messageObject.js
+++ 2-labbymezzage/js/messageObject.js
@@ -1,7 +1,12 @@
 var messageApp = {
 
 	init: function(){
-	    alert("");
+	    //Instansierar nytt Message-objekt
+	    var mess = new Message("Testmeddelande", new Date());
+	    //alert(mess); //Skriver ut meddelandet mha toString
+	    alert(mess.getText()); //Skriver ut endast texten
+	    mess.setText("En annan text"); //Skickar in en ny messagesträng
+	    alert(mess.getText()); //Testar message med nya texten
 	}
 	
 }
