d978ad1f080622039d56dc37744ab68ff61dbcb5
lindaott <lo222hd@student.lnu.se>
2014-11-30 12:02:45 +0000

    2.5 done

--- 2-labbymezzage/js/messageObject.js
+++ 2-labbymezzage/js/messageObject.js
@@ -3,22 +3,20 @@ var messageApp = {
     messages: [],
 
 	init: function(){
+		var messageFromForm = document.getElementById("message-area");
 		//Instansierar nytt Message-objekt
-		var mess0 = new Message("Testmeddelande 1", new Date());
-		var mess1 = new Message("Testmeddelande 2", new Date());
+		var mess = new Message(messageFromForm.value, new Date());
 
 		//Lägger till meddelandet till arrayen messages
-		messageApp.messages.push(mess0);
-		messageApp.messages.push(mess1);
+		messageApp.messages.push(mess);
 		   
-		var writeMessage = messageApp.messages[0].getText();
-		var writeMessage2 = messageApp.messages[1].getText();
+		var writeMessage = messageApp.messages[0].getText() + " (" + messageApp.messages[0].getDate() + " )";
 		   
-		//alert(writeMessage);
+		alert(writeMessage);
 		//alert(writeMessage2);
 	}
 	
 }
 
 // Starta applikationen när fönstret laddat klart
-window.onload = messageApp.init;  
+//window.onload = messageApp.init;  
