4f791d493264dea6587bc7c2bb5f11c5126879ad
lindaott <lo222hd@student.lnu.se>
2014-12-01 09:02:34 +0000

    2.6 done

--- 2-labbymezzage/js/messageObject.js
+++ 2-labbymezzage/js/messageObject.js
@@ -1,21 +1,62 @@
 var messageApp = {
     
     messages: [],
-
-	init: function(){
-		var messageFromForm = document.getElementById("message-area");
+    
+    click: function() {
+    	
+    	var messageArea = document.getElementById("message-area");
 		//Instansierar nytt Message-objekt
-		var mess = new Message(messageFromForm.value, new Date());
+		var mess = new Message(messageArea.value, new Date());
 
 		//Lägger till meddelandet till arrayen messages
 		messageApp.messages.push(mess);
-		   
-		var writeMessage = messageApp.messages[0].getText() + " (" + messageApp.messages[0].getDate() + " )";
-		   
-		alert(writeMessage);
-		//alert(writeMessage2);
+		
+		//Uppdaterar HTML med antal meddelanden
+		var numberMessages = messageApp.messages.length;
+
+		var numberMessagesHTML = document.getElementById("number-messages");
+		
+		numberMessagesHTML.innerHTML = numberMessages;
+		
+		messageApp.renderMessages();
+    	
+    },
+    
+    renderMessages: function(){
+    	
+    	//Rensar först div
+		var div = document.getElementById("writeout");
+     	div.innerHTML = "";
+    	
+    	//Uppdaterar HTML med antal meddelanden
+		var numberMessages = messageApp.messages.length;
+		//Loopar igenom arrayen och skriver ut samtliga meddelanden
+    	//mha renderMessage
+    	var i;
+    	
+		for (i=0; i < numberMessages; i++) {
+			messageApp.renderMessage(i);
+    	}
+    	
+    },
+
+	renderMessage: function(messageID) {
+		var numberMessages = messageApp.messages.length;
+		
+		if (messageID === undefined) {
+			messageID = numberMessages - 1;
+		}
+		
+		var text = document.createElement("p");
+		text.innerHTML = messageApp.messages[messageID].getDate() + ": " + messageApp.messages[messageID].getText();
+		var messageWriteOut = document.getElementById("writeout");
+		messageWriteOut.appendChild(text);
+
+		
+		return false;
 	}
-	
+
+
 }
 
 // Starta applikationen när fönstret laddat klart
