5d874b630b18248abce095418b967d7f03836b20
lindaott <lo222hd@student.lnu.se>
2015-01-06 12:26:56 +0000

    L03 Board set

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -6,13 +6,39 @@ var Memory = {
     randomArray: [],
     
     init: function() {
-
-        this.randomArray = RandomGenerator.getPictureArray(4,4);
         
-        alert(this.randomArray.length);
-        //Skriver ut arrayen
-        for (var i=0; i<this.randomArray.length; i++) {
-            console.log(this.randomArray[i]);
+        var rows = 4;
+        var cols = 3;
+
+        this.randomArray = RandomGenerator.getPictureArray(rows,cols);
+        
+        var colsCount = 1;
+        
+        //Skriver ut en tabell om arrayens längd är större än 1
+        if (this.randomArray.length > 0) {
+            //Hämtar en referens till div-taggen där tabellen ska vara
+            var tableDiv = document.getElementById("tableDiv");
+            var tableHTML = "<table>";
+            for (var i=0; i<this.randomArray.length; i++) {
+                //Lägger till en tr-tagg om colsCount % cols = 0
+                if (colsCount % cols === 1) {
+                    tableHTML += "<tr>"; 
+                }
+                //Lägger till en td-tagg och numret
+                var imgNumber = this.randomArray[i];
+                //tableHTML += "<td><a href=\""+this.randomArray[i]+".png\"><img src=\"pics/0.png\" alt=\"Click me!\"></a></td>";
+                tableHTML += "<td><a href=\""+this.randomArray[i]+".png\"><img src=\"pics/"+this.randomArray[i]+".png\" alt=\"Click me!\"></a></td>";
+                
+                //Lägger till en avslutande tr-tagg om colsCount % cols = 0
+                if (colsCount % cols === 0) {
+                    tableHTML += "</tr>"; 
+                }
+
+                colsCount++;
+                
+            }
+            //Skriver ut tabellen
+            tableDiv.innerHTML = tableHTML;
         }
     }
     
