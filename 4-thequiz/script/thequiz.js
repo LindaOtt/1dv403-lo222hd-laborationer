28c77f823b0b975e769f48300f49d8bc44e11a23
lindaott <lo222hd@student.lnu.se>
2015-01-17 00:17:38 +0000

    L04 question-answer working

--- 4-thequiz/script/thequiz.js
+++ 4-thequiz/script/thequiz.js
@@ -37,57 +37,19 @@ var TheQuiz = {
     
     responseId: 0,
     
+    responseObject: {},
+    
     question: "Empty",
     
     nextURL: "",
     
-    init: function() {
+    init: function(url) {
     
         //Hämtar frågan
-        var url = "http://vhost3.lnu.se:20080/question/1";
-        TheQuiz.getInfo(url, TheQuiz.saveResponse);
-        
-         //Hämtar värdet från response
-        //alert("Length: " + TheQuiz.response.length);
-        /*
-        TheQuiz.response.forEach(function(item){
-            console.log("Id: " + item.id);
-            console.log("Question: " + item.question);
-        });
-        */
-        
-        var responseLength = TheQuiz.response.length;
-        
-        //TheQuiz.writeQuestion();
-        
-        TheQuiz.response.forEach(function(item){
-            var questionGet = item.question;
-            alert("questionGet: " + questionGet);
-            var questionText = document.getElementById("question");
-            questionText.innerHTML = questionGet;
-        });
-        
-       
-        console.log("Init: TheQuiz.response[0]: " + TheQuiz.response);
-    
+        TheQuiz.getQuestion(url);
+
         var nextURL;
-        
-        
-        //Skriver ut formuläret
-        //if (TheQuiz.writeForm()) {
-            TheQuiz.writeForm();
-            var submitButton = document.getElementById("submitbutton");
-            submitButton.addEventListener("click", function(){
-                //Skicka iväg informationen till servern
-                TheQuiz.sendResponse();
-            });
-        //}
-        //else {
-            //alert("Det gick inte att skriva ut formuläret.");
-        //}
-        
-        
-    
+ 
     },
     
     saveResponse: function(data) {
@@ -95,21 +57,65 @@ var TheQuiz = {
             TheQuiz.response.push(data);
     },
     
-    writeQuestion: function() {
-        alert("TheQuiz length: " + TheQuiz.response.length);
-        TheQuiz.response.forEach(function(item){
-            var questionGet = item.question;
-            alert("questionGet: " + questionGet);
+    writeAll: function(response) {
+            //Skriver ut frågan
             var questionText = document.getElementById("question");
-            questionText.innerHTML = questionGet;
-        });
+            questionText.innerHTML = response.question;
+            
+            //Skriver ut formuläret
+            TheQuiz.writeForm();
+            
+            //Skapa en lyssnare på knappen
+            document.getElementById("submitbutton").addEventListener("click", function(e){
+                e.preventDefault();
+                var nextURL = response.nextURL;
+                console.log("nextURL: " + nextURL);
+                
+                //Skicka användarens svar i JSON-format till servern
+                var xhr2 = new XMLHttpRequest(); //skapat requestobjekt
+        
+        		xhr2.onreadystatechange = function(){
+        
+        			if(xhr2.readyState === 4 && xhr2.status === 200) { //Allt ok med anropet och vi fick tillbaka statuskod 200
+        				//Användaren svarade rätt!
+        				console.log(xhr2.responseText);
+        				
+        				//Ladda om med ny fråga och url
+        			}
+        			else {
+        			    //Användaren svarade fel!
+        			    console.log(xhr2.responseText);
+        			}
+        
+        		};
+        		
+        		
+        		
+        		xhr2.open('POST', nextURL, true); 
+        
+        		xhr2.setRequestHeader('Content-Type', 'application/json'); //Talar om för webbservern att det är i JSON-formatet vi skickar.
+        		
+        		//Hämtar innehållet i formuläret
+        		var answer = document.getElementById("answer");
+        		var value = answer.value;
+        		
+        		//Skapar ett objekt av värdena
+        		TheQuiz.responseObject.answer = value;
+                        		
+        		//Omvandlar svaret till JSON-format
+        		answer = JSON.stringify(TheQuiz.responseObject);
+        		console.log("answer: " + answer);
+        		
+        		//Skickar svaret
+        		xhr2.send(answer);
+            });
     },
 
     
     //Funktion som hämtar info
     //url är dit requestobjektet ska skickas,
     //infotyp är den typ av info som efterfrågas
-    getInfo: function(url, callback) {
+    getQuestion: function(url) {
 
     
         var xhr = new XMLHttpRequest(); //skapat requestobjekt
@@ -119,11 +125,12 @@ var TheQuiz = {
 		xhr.onreadystatechange = function(){
 
 			if(xhr.readyState === 4 && xhr.status === 200) { //Allt ok med anropet och vi fick tillbaka statuskod 200
-				var responseObject = JSON.parse(xhr.responseText);
-				callback(responseObject);
+				var response = JSON.parse(xhr.responseText);
+				
+				TheQuiz.writeAll(response);
 				
 				//Sparar response i egenskap på objektet
-                TheQuiz.saveResponse(responseObject);
+                //TheQuiz.saveResponse(responseObject);
 				
 			}
 
@@ -163,180 +170,10 @@ var TheQuiz = {
         
     },
     
-    sendResponse: function() {
-
-        
-        //var nextURL =  "http://vhost3.lnu.se:20080/answer/1/";
-        
-        TheQuiz.response.forEach(function(item){
-            var response = item.response;
-            var nextURL = item.nextURL;
-        });
-       
-        document.getElementById("submitbutton").addEventListener("click", function(){
-            //Hämtar värdet från input-fältet i formuläret
-            //var answer = document.getElementById("answer");
-
-            
-            var answer = {
-                "answer": 2
-            };
-            
-            
-
-            //Omvandla användarens svar till JSON-format
-            answer = JSON.stringify(answer);
-            
-            console.log("answer: " + answer);
-            
-            
-            //Skicka användarens svar i JSON-format till servern
-            var xhr2 = new XMLHttpRequest(); //skapat requestobjekt
-    
-    		xhr2.onreadystatechange = function(){
-    
-    			if(xhr2.readyState === 4 && xhr2.status === 200) { //Allt ok med anropet och vi fick tillbaka statuskod 200
-    				console.log("xhr2.responseText: " + xhr2.responseText);
-    			}
-    			else {
-    			    console.log("xhr2.readyState: " + xhr2.readyState);
-    			}
-    
-    		};
-    		
-    		console.log("nextURL 2: " + nextURL);
-    		
-    		xhr2.open('POST', nextURL, true); 
-    
-    		xhr2.setRequestHeader('Content-Type', 'application/json'); //Talar om för webbservern att det är i JSON-formatet vi skickar.
-    
-    		xhr2.send(answer);
-    		
-        });
-
-    },
-    
-    
-    /*
-    function writeResponse(response) {
-        console.log("Id: " + response.id);
-        console.log("Question: " + response.question);
-        console.log("nextURL: " + response.nextURL);
-        
-        var nextURL = response.nextURL;
-        
-        /* Skriver ut frågan 
-		var question = response.question;
-		var questionText = document.getElementById("question");
-        questionText.innerHTML = question;
-        
-        document.getElementById("submitButton").addEventListener("click", function(){
-            //Hämtar värdet från input-fältet i formuläret
-            var answer = document.getElementById("answer").value;
-
-            //Omvandla användarens svar till JSON-format
-            answer = JSON.stringify(answer);
-            console.log("answer: " + answer);
-            
-            
-            //Skicka användarens svar i JSON-format till servern
-            var xhr2 = new XMLHttpRequest(); //skapat requestobjekt
-    
-    		xhr2.onreadystatechange = function(){
-    
-    			if(xhr2.readyState === 4 && xhr2.status === 200) { //Allt ok med anropet och vi fick tillbaka statuskod 200
-    				console.log("xhr2.responseText: " + xhr2.responseText);
-    			}
-    			else {
-    			    console.log("xhr2.readyState: " + xhr2.readyState);
-    			}
-    
-    		};
-    		
-    		console.log("nextURL 2: " + nextURL);
-    		
-    		xhr2.open('POST', nextURL, true); 
-    
-    		xhr2.setRequestHeader('Content-Type', 'application/json'); //Talar om för webbservern att det är i JSON-formatet vi skickar.
-    
-    		xhr2.send(answer);
-    		
-        });
-
-    }
-    
-    */
-    
-    /*
-    document.getElementById("submitButton").addEventListener("click", function(){
-        
-        
-        
-        var xhr2 = new XMLHttpRequest(); //skapat requestobjekt
-	    
-	    xhr2.onreadystatechange = function(){
-                alert("Ready");
-    			if(xhr2.readyState === 4 && xhr2.status === 200) { //Allt ok med anropet och vi fick tillbaka statuskod 200
-    				var response = JSON.parse(xhr2.responseText);
-    				var nextURL = response.nextURL;
-    				alert(nextURL);
-    				console.log("nextURL: " + nextURL);
-    			}
-    			else {
-    			    alert("xhr2.readyState: " + xhr2.readyState);
-    			    alert("xhr2.status: " + xhr2.status);
-    			}
-
-    	};
-    	
-    	
-    
-    	xhr2.open("GET", url, true);
-    	
-    
-    	xhr2.send(null);
-
-    }); // Hämtat ut knappen och kopplat en event-hanterare på samma rad.
-    */
-    
-    /*
-    document.getElementById("submitButton").addEventListener("click", function(){
-    	    
-    	    //Hämtar användarens svar från formuläret
-    	    var answer = document.getElementById("answer");
-    	    
-    	    //Omvandlar svaret till JSON-format
-    	    var answerJSON = JSON.stringify(answer);
-    	    
-    		
-    		var xhr2 = new XMLHttpRequest(); //skapat requestobjekt
-    
-    		xhr2.onreadystatechange = function(){
-    
-    			//console.log("svar");
-    
-    			if(xhr2.readyState === 4 && xhr2.status === 200) { //Allt ok med anropet och vi fick tillbaka statuskod 200
-    				console.log(xhr.responseText);
-    				var nextURL = JSON.parse(xhr2.responseText);
-    				alert("nextURL: " + nextURL);
-    				//questionText.appendChild(question);
-    			}
-    
-    		};
-    		
-    		//console.log("nextURL: " + submitButton.nextURL);
-    		
-    		xhr2.open('POST', nextURL, true); //POST skickar data till filen setProduct.php, gör det synkront = true
-    
-    		xhr2.setRequestHeader('Content-Type', 'application/json'); //Talar om för webbservern att det är i JSON-formatet vi skickar.
-    
-    		//xhr.send(answerJSON);
-    
-    }); // Hämtat ut knappen och kopplat en event-hanterare på samma rad.
-    */
+   
     
     submitData: function(nextURL) {
-         //Hämtar användarens svar från formuläret
+        //Hämtar användarens svar från formuläret
 	    var answer = document.getElementById("answer");
 	    
 	    //Omvandlar svaret till JSON-format
@@ -370,4 +207,4 @@ var TheQuiz = {
 
 };
 
-window.onload = TheQuiz.init;
+window.onload = TheQuiz.init("http://vhost3.lnu.se:20080/question/1");
